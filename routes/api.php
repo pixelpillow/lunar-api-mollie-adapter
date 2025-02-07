d0e2e69899f4a78cf8814e1603757bb28d9c2908
Thomas van der Westen <thomas@pixelpillow.nl>
2025-02-07 16:46:17 +0100

    Update route configuration keys from 'dystore' to 'lunar-api' for consistency

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,6 @@ use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Route;
 
 Route::group([
-    'prefix' => Config::get('dystore.general.route_prefix'),
-    'middleware' => Config::get('dystore.general.route_middleware'),
-], fn () => RegisterRoutesFromConfig::run('dystore.mollie.domains'));
+    'prefix' => Config::get('lunar-api.general.route_prefix'),
+    'middleware' => Config::get('lunar-api.general.route_middleware'),
+], fn () => RegisterRoutesFromConfig::run('lunar-api.mollie.domains'));
