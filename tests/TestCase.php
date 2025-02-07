60b33c70c88568b10d54ec181e9104c5f13e7fd3
Thomas van der Westen <thomas@pixelpillow.nl>
2025-02-07 16:50:48 +0100

    Update TestCase configuration keys from 'dystore' to 'lunar-api' for consistency

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -131,9 +131,9 @@ class TestCase extends Orchestra
         $app->useEnvironmentPath(__DIR__.'/..');
         $app->bootstrapWith([LoadEnvironmentVariables::class]);
 
-        Config::set('dystore.mollie.mollie_key', 'test_G3ys6guxc9Su7VJ2xctR4N4VqvGbQR');
-        Config::set('dystore.mollie.redirect_url_generator', TestRedirectGenerator::class);
-        Config::set('dystore.mollie.cancel_url_generator', TestRedirectGenerator::class);
+        Config::set('lunar-api.mollie.mollie_key', 'test_G3ys6guxc9Su7VJ2xctR4N4VqvGbQR');
+        Config::set('lunar-api.mollie.redirect_url_generator', TestRedirectGenerator::class);
+        Config::set('lunar-api.mollie.cancel_url_generator', TestRedirectGenerator::class);
 
         /**
          * Lunar configuration
